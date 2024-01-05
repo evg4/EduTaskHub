f7ec8668e37b28e7d922dcf22bcc13d461100807
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-01-05 20:48:21 +0000

    Add Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+function Footer() {
+  let now = new Date();
+  //let dateTime = now.toLocaleDateString();
+  let year = now.getFullYear();
+  let month = (now.getMonth() + 1).toString().padStart(2, "0");
+  let day = now.getDate().toString().padStart(2, "0");
+  let fullDate = `${year}-${month}-${day}`;
+  console.log(fullDate);
+  return (
+    <footer>
+      <p>Today's date: {fullDate}</p>
+    </footer>
+  );
+}
+
+export default Footer;
