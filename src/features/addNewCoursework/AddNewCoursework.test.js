6e4764a6393312cd0c588df09a02d26117e1ab72
Elizabeth Gardiner <liz.gardiner@btinternet.com>
2024-01-15 19:37:44 +0000

    comment out unused variables

--- src/features/addNewCoursework/AddNewCoursework.test.js
+++ src/features/addNewCoursework/AddNewCoursework.test.js
@@ -1,11 +1,11 @@
 import AddNewCoursework from "./AddNewCoursework";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import userEvent from "@testing-library/user-event";
+//import userEvent from "@testing-library/user-event";
 import React from "react";
 import { store } from "../../app/store";
-import { toggleView } from "./addNewCourseworkSlice";
-import { Provider } from "react-redux";
+//mport { toggleView } from "./addNewCourseworkSlice";
+//import { Provider } from "react-redux";
 
 it("renders a button", () => {
   const state = store.getState();
